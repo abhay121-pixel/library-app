fe5872601687640b04eb0bbcf2b4469cdb2bed83
Abhay Singh <82321625+abhay121-pixel@users.noreply.github.com>
2024-03-12 22:59:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 app=Flask( __name__)
-@app.route('/')
-def index():
-    return render_template('login.html')
+import config
+import routes 
 if __name__=='__main__':
     app.run(debug=True)
